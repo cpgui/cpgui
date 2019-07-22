bebf44e1dce14c9db4bf6a5aa0b2e94f9594c464
CodeDoctorDE <proficodergames@gmail.com>
2019-07-22 18:09:50 +0200

    add some files

---
+++ app.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+puts 'Loading app file...'
+
+# The main class of the app
+class CPGUI
+  def initialize; end
+
+  def run; end
+end
+puts 'Successfully loaded app file!'

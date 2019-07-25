31e020de85d3a4fa36703d1896702aeaac70f4d5
CodeDoctorDE <proficodergames@gmail.com>
2019-07-25 10:04:48 +0200

    fix bugs

--- modules/main/console.rb
+++ modules/main/console.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'json'
 # Main module for the cpgui
 class MainModule
@@ -6,9 +7,9 @@ class MainModule
     return true if module_console(command, args)
 
     case command
-    when 'exit', 'quit', 'close', 'end', 'stop' then on_exit
-    when 'info', 'information', 'cpgui' then on_info
-    when 'main', 'main-module' then on_main
+    when 'exit', 'quit', 'close', 'end', 'stop' then command_exit
+    when 'info', 'information', 'cpgui' then command_info
+    when 'main', 'main-module' then command_main
     else return false
     end
     true
@@ -16,10 +17,10 @@ class MainModule
 
   def module_console(command, args)
     case command
-    when 'modules', 'module', 'mods', 'mod', 'h', 'help' then on_modules
-    when 'r', 'rl', 'rel', 'reload', 'ref', 'refresh' then on_reload
-    when 'disable', 'disable-module', 'disable-modules' then on_module_disable(args)
-    when 'enable', 'enable-module', 'enable-modules' then on_module_enable
+    when 'modules', 'module', 'mods', 'mod', 'h', 'help' then command_modules
+    when 'r', 'rl', 'rel', 'reload', 'ref', 'refresh' then command_reload
+    when 'disable', 'disable-module' then command_disable(args)
+    when 'enable', 'enable-module' then command_enable(args)
     else return false
     end
     true
@@ -31,32 +32,35 @@ class MainModule
         module_class = Kernel.const_get(args[0])
         return module_class if module_class < CPGUI::AppModule
 
-        error 'Invalid module 1!'
+        error 'Invalid module!'
       rescue StandardError then error 'Invalid module!' end
     else
-      error('Usage: disable <Module>!')
+      error 'Usage: disable <Module>!'
     end
   end
 
-  def on_module_disable(args)
+  def command_disable(args)
     module_class = can_disable args
     return unless module_class
 
     disable_message = 'Please use confirm if you really want to disable this!'
     on_self = module_class == self.class && args.length != 2
-    send(disable_message) && (return false) if on_self
-    @module_manager.disable(module_class)
+    if on_self
+      send(disable_message)
+    else
+      @module_manager.disable(module_class)
+    end
   end
 
-  def on_main
+  def command_main
     send Rainbow("main-#{version}").white
   end
 
-  def on_info
+  def command_info
     send Rainbow("cpgui-#{version}").white
   end
 
-  def on_exit
+  def command_exit
     puts "\r\n"
     send Rainbow('Exiting application...').red
     exit
@@ -74,14 +78,14 @@ class MainModule
     module_classes
   end
 
-  def on_modules
+  def command_modules
     module_classes = modules
     module_string = module_classes.join(Rainbow(', ').silver)
     output = Rainbow("#{@module_manager.modules.length} module(s): ").aqua
     send output + module_string
   end
 
-  def on_reload
+  def command_reload
     send Rainbow('Reloading cpgui...').yellow
     @module_manager.stop
     @module_manager.start
